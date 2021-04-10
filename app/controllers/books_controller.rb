f2d02d110130ca6a6234a28d31956b96a8a97f29
Imkn1018 <o2oneru2505@gmail.com>
2021-04-10 08:36:36 +0000

    [Finish] validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,15 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
   def show
     @book = Book.find(params[:id])
   end
 
-  def index
-    @books = Book.all
-  end
-
   def new
-    @book = Book.new
   end
 
   def create
@@ -17,18 +17,23 @@ class BooksController < ApplicationController
     if @book.save
      flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render action: :index
     end
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(book)
+    else
+      render :edit
     end
   end
 
@@ -41,9 +46,9 @@ class BooksController < ApplicationController
   end
 
   private
-  
+
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :body)
   end
-  
+
 end
